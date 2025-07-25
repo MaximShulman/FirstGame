a8cf2edce8a1389ac07654c77bab2b2b46bfe80d
maxim <dummy@nowhere.com>
2025-07-25 15:46:06 +0200

    Tutorial finished

--- core/src/main/java/shulman/maxim/firstgame/Main.java
+++ core/src/main/java/shulman/maxim/firstgame/Main.java
@@ -2,81 +2,172 @@ package shulman.maxim.firstgame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import java.util.ArrayList;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
 
-    Texture backgroundTexture;
-    Texture bucketTexture;
-    Texture dropTexture;
-    Sound dropSound;
-    Music music;
+//    Texture backgroundTexture;
+//    Texture bucketTexture;
+//    Texture dropTexture;
+//    Sound dropSound;
+//    Music music;
+//    Sprite bucketSprite; // Declare a new Sprite variable
+//
+//    Vector2 touchPos;
+//    Rectangle bucketRectangle;
+//    Rectangle dropRectangle;
+//    SpriteBatch spriteBatch;
+//    FitViewport viewport;
+//    ArrayList<Sprite> dropSprites;
+//    float dropTimer;
+//
+//    @Override
+//    public void create() {
+//
+//        backgroundTexture = new Texture("background.png");
+//        bucketTexture = new Texture("bucket.png");
+//        dropTexture = new Texture("drop.png");
+//        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
+//        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+//        spriteBatch = new SpriteBatch();
+//        viewport = new FitViewport(8, 5);
+//        bucketSprite = new Sprite(bucketTexture); // Initialize the sprite based on the texture
+//        bucketSprite.setSize(1, 1); // Define the size of the sprite
+//        touchPos = new Vector2();
+//        dropSprites = new ArrayList<>();
+//        bucketRectangle = new Rectangle();
+//        dropRectangle = new Rectangle();
+//        music.setLooping(true);
+//        music.setVolume(.5f);
+//        music.play();
+//    }
+//    @Override
+//    public void resize(int width, int height) {
+//        viewport.update(width, height, true);
+//    }
+//
+//
+//    @Override
+//    public void render() {
+//        input();
+//        logic();
+//        draw();
+//    }
+//
+//    private void input() {
+//        float speed = 1.75f;
+//        float delta = Gdx.graphics.getDeltaTime(); // retrieve the current delta
+//        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+//            bucketSprite.translateX(speed * delta); // Move the bucket right
+//        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+//            bucketSprite.translateX(-speed * delta); // move the bucket left
+//        }
+//
+//        if (Gdx.input.isTouched()) {
+//            touchPos.set(Gdx.input.getX(), Gdx.input.getY()); // Get where the touch happened on screen
+//            viewport.unproject(touchPos); // Convert the units to the world units of the viewport
+//            bucketSprite.setCenterX(touchPos.x); // Change the horizontally centered position of the bucket
+//        }
+//    }
+//
+//    private void logic() {
+//        // Store the worldWidth and worldHeight as local variables for brevity
+//        float worldWidth = viewport.getWorldWidth();
+//        float worldHeight = viewport.getWorldHeight();
+//
+//        // Store the bucket size for brevity
+//        float bucketWidth = bucketSprite.getWidth();
+//        float bucketHeight = bucketSprite.getHeight();
+//
+//        // Subtract the bucket width
+//        bucketSprite.setX(MathUtils.clamp(bucketSprite.getX(), 0, worldWidth - bucketWidth));
+//
+//        float delta = Gdx.graphics.getDeltaTime(); // retrieve the current delta
+//
+//        bucketRectangle.set(bucketSprite.getX(), bucketSprite.getY(), bucketWidth, bucketHeight);
+//
+//        for (int i = dropSprites.size() - 1; i >= 0; i--) {
+//            Sprite dropSprite = dropSprites.get(i);
+//            float dropWidth = dropSprite.getWidth();
+//            float dropHeight = dropSprite.getHeight();
+//
+//            dropSprite.translateY(-2f * delta);
+//            // Apply the drop position and size to the dropRectangle
+//            dropRectangle.set(dropSprite.getX(), dropSprite.getY(), dropWidth, dropHeight);
+//
+//            if (dropSprite.getY() < -dropHeight) dropSprites.remove(i);
+//            else if (bucketRectangle.overlaps(dropRectangle)) { // Check if the bucket overlaps the drop
+//                dropSprites.remove(i); // Remove the drop
+//                dropSound.play(); // Play the sound
+//            }
+//        }
+//
+//        dropTimer += delta; // Adds the current delta to the timer
+//        if (dropTimer > 1f) { // Check if it has been more than a second
+//            dropTimer = 0; // Reset the timer
+//            createDroplet(); // Create the droplet
+//        }
+//
+//    }
+//
+//    private void draw() {
+//        ScreenUtils.clear(Color.BLACK);
+//        viewport.apply();
+//        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+//        spriteBatch.begin();
+//        // store the worldWidth and worldHeight as local variables for brevity
+//        float worldWidth = viewport.getWorldWidth();
+//        float worldHeight = viewport.getWorldHeight();
+//        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight); // draw the background
+//        bucketSprite.draw(spriteBatch); // Sprites have their own draw method
+//        // draw each sprite
+//        for (Sprite dropSprite : dropSprites) {
+//            dropSprite.draw(spriteBatch);
+//        }
+//        spriteBatch.end();
+//    }
+//    private void createDroplet() {
+//        // create local variables for convenience
+//        float dropWidth = 1;
+//        float dropHeight = 1;
+//        float worldWidth = viewport.getWorldWidth();
+//        float worldHeight = viewport.getWorldHeight();
+//
+//        // create the drop sprite
+//        Sprite dropSprite = new Sprite(dropTexture);
+//        dropSprite.setSize(dropWidth, dropHeight);
+//        dropSprite.setY(8);
+//        dropSprite.setX(MathUtils.random(0f, worldWidth - dropWidth)); // Randomize the drop's x position        dropSprite.setY(worldHeight);
+//        dropSprites.add(dropSprite); // Add it to the list
+//    }
+//
+//    @Override
+//    public void pause() {
+//        super.pause();
+//    }
+//
+//    @Override
+//    public void resume() {
+//        super.resume();
+//    }
+//
+//    @Override
+//    public void dispose() {
+//
+//    }
 
-
-
-    SpriteBatch spriteBatch;
-    FitViewport viewport;
-
-    @Override
-    public void create() {
-        backgroundTexture = new Texture("background.png");
-        bucketTexture = new Texture("bucket.png");
-        dropTexture = new Texture("drop.png");
-        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
-        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
-        spriteBatch = new SpriteBatch();
-        viewport = new FitViewport(8, 5);
-    }
-    @Override
-    public void resize(int width, int height) {
-        viewport.update(width, height, true);
-    }
-
-
-    @Override
-    public void render() {
-        input();
-        logic();
-        draw();
-    }
-
-    private void input() {
-
-    }
-
-    private void logic() {
-
-    }
-
-    private void draw() {
-        ScreenUtils.clear(Color.BLACK);
-        viewport.apply();
-        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
-        spriteBatch.begin();
-        spriteBatch.draw(bucketTexture, 0, 0, 1, 1); // draw the bucket with width/height of 1 meter
-        spriteBatch.end();
-    }
-
-    @Override
-    public void pause() {
-        super.pause();
-    }
-
-    @Override
-    public void resume() {
-        super.resume();
-    }
-
-    @Override
-    public void dispose() {
-
-    }
 }
